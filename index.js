8f95eef2bc9eac5d3c46fd8720b06f30963e38ad
Gianthra <giarobson@googlemail.com>
2019-11-07 12:30:17 +0000

    Reduced amount of code
    This could also be reduced further by replacing `String.prototype` with `""` in in all instances except for when `.trimEnd` is set

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-String.prototype.trimEnd = String.prototype.trimEnd ? String.prototype.trimEnd : function() {
-	if(String.prototype.trimRight) {
-		return this.trimRight();
-	} else if(String.prototype.trim) {
-		var trimmed = this.trim();
-		var indexOfWord = this.indexOf(trimmed);
-		
-		return this.slice(indexOfWord, this.length);
-	}
-};
+if(!String.prototype.trimEnd){
+	String.prototype.trimEnd 
+		= String.prototype.trimRight 
+		? String.prototype.trimRight
+		: function() { return this.replace(/\s+$/); }
+}
